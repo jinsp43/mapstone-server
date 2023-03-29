f652e3844dc78753f4349785f81cc7ba3b3ed981
jinsp43 <jinsp43@gmail.com>
2023-03-29 17:39:56 +0100

    set up migration to create all tables

--- knexfile.js
+++ knexfile.js
@@ -1,47 +1,11 @@
-// Update with your config settings.
+require("dotenv").config();
 
-/**
- * @type { Object.<string, import("knex").Knex.Config> }
- */
 module.exports = {
-
-  development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    }
+  client: "mysql2",
+  connection: {
+    host: process.env.DB_LOCAL_HOST,
+    database: process.env.DB_LOCAL_DBNAME,
+    user: process.env.DB_LOCAL_USER,
+    password: process.env.DB_LOCAL_PASSWORD,
   },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
 };
---
+++ migrations/20230329154905_create_all_tables.js
@@ -0,0 +1,60 @@
+exports.up = function (knex) {
+  return knex.schema
+    .createTable("user", (table) => {
+      table.increments("id").primary();
+      table.integer("google_id");
+      table.string("username").notNullable();
+      table.string("marker_colour").defaultTo("orange");
+      table.timestamp("created_at").defaultTo(knex.fn.now());
+    })
+    .createTable("group", (table) => {
+      table.increments("id").primary();
+      table.string("group_name").notNullable();
+      table.timestamp("created_at").defaultTo(knex.fn.now());
+    })
+    .createTable("user_group", (table) => {
+      table.increments("id").primary();
+      table.integer("user_id").unsigned().notNullable();
+      table.integer("group_id").unsigned().notNullable();
+      table
+        .foreign("user_id")
+        .references("id")
+        .inTable("user")
+        .onUpdate("CASCADE")
+        .onDelete("CASCADE");
+      table
+        .foreign("group_id")
+        .references("id")
+        .inTable("group")
+        .onUpdate("CASCADE")
+        .onDelete("CASCADE");
+    })
+    .createTable("marker", (table) => {
+      table.increments("id").primary();
+      table.integer("user_id").unsigned().notNullable();
+      table.integer("group_id").unsigned().notNullable();
+      table.string("title").notNullable();
+      table.decimal("longitude", 4).notNullable();
+      table.decimal("latitude", 4).notNullable();
+      table
+        .foreign("user_id")
+        .references("id")
+        .inTable("user")
+        .onUpdate("CASCADE")
+        .onDelete("CASCADE");
+      table
+        .foreign("group_id")
+        .references("id")
+        .inTable("group")
+        .onUpdate("CASCADE")
+        .onDelete("CASCADE");
+    });
+};
+
+exports.down = function (knex) {
+  return knex.schema
+    .dropTable("marker")
+    .dropTable("user_group")
+    .dropTable("group")
+    .dropTable("user");
+};
