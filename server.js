b106e9da29bdf2891f7a55d11de1175d3dc8523a
jinsp43 <jinsp43@gmail.com>
2023-03-29 22:57:42 +0100

    place marker images in public

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
 const express = require("express");
+const cors = require("cors");
 
+require("dotenv").config();
 const PORT = process.env.PORT || 5050;
+const { FRONTEND_URL } = process.env;
+
+const app = express();
+app.use(cors({ origin: FRONTEND_URL }));
+app.use(express.static("public"));
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
