50f74319f83172a3b3a5345977e2004857513dfe
jinsp43 <jinsp43@gmail.com>
2023-03-30 14:42:02 +0100

    add passwords for the seeded users

--- seed_data/usersData.js
+++ seed_data/usersData.js
@@ -2,40 +2,48 @@ module.exports = [
   {
     id: 1,
     username: "JinAndTonic",
+    password: "mapstone",
   },
   {
     id: 2,
     username: "Shirby",
     marker_colour: "pink",
+    password: "mapstone",
   },
   {
     id: 3,
     username: "Seal",
     marker_colour: "grey",
+    password: "mapstone",
   },
   {
     id: 4,
     username: "Cheesed",
     marker_colour: "blue",
+    password: "mapstone",
   },
   {
     id: 5,
     username: "JonnyDaBest",
     marker_colour: "purple",
+    password: "mapstone",
   },
   {
     id: 6,
     username: "Harley",
     marker_colour: "blue",
+    password: "mapstone",
   },
   {
     id: 7,
     username: "Yulia",
     marker_colour: "light blue",
+    password: "mapstone",
   },
   {
     id: 8,
     username: "Anton",
     marker_colour: "dark blue",
+    password: "mapstone",
   },
 ];
